1a6fcbc1fed860827e6735f6709ae28a5d98c80b
Ananda Rizky <anandarizkysetyanu29@gmail.com>
2025-10-25 22:17:56 +0700

    Menambahkan index.php untuk pengawalan saja

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Praktikum</title>
+</head>
+<body>
+    <h1>Mau Bikin Apa Yah</h1>
+</body>
+</html>
